a8fa7321acb5f0bc69f3345f29671a6669684f91
r0cc4t0 <guilherme.roccato@outlook.com>
2020-07-11 17:46:52 -0300

    HTTP POST, PUT and DELETE Methods

--- index.php
+++ index.php
@@ -19,4 +19,25 @@ $app->get('/products[/{name}]', function (Request $request, Response $response, 
     return $response;
 });
 
+$app->post('/products', function (Request $request, Response $response, array $args): Response {
+    $data = $request->getParsedBody();
+    $name = $data['name'] ?? 'Mouse';
+    $response->getBody()->write("Product: {$name}");
+    return $response;
+});
+
+$app->put('/products', function (Request $request, Response $response, array $args): Response {
+    $data = $request->getParsedBody();
+    $name = $data['name'] ?? 'RAM';
+    $response->getBody()->write("Product: {$name}");
+    return $response;
+});
+
+$app->delete('/products', function (Request $request, Response $response, array $args): Response {
+    $data = $request->getParsedBody();
+    $name = $data['name'] ?? 'HD';
+    $response->getBody()->write("Product: {$name}");
+    return $response;
+});
+
 $app->run();
