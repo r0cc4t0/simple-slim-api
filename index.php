393ce13fc2f8964ce5899a0cdd8fec9fe82314f6
r0cc4t0 <guilherme.roccato@outlook.com>
2020-07-11 17:03:04 -0300

    HTTP GET Method

--- index.php
+++ index.php
@@ -1,11 +1,22 @@
 <?php
 
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
 require_once 'vendor/autoload.php';
 
 $app = new \Slim\App();
 
-$app->get('/', function ($request, $response, $args) {
-    return $response->getBody()->write('Hello, World!');
+$app->get('/', function (Request $request, Response $response, array $args): Response {
+    $response->getBody()->write('Hello, World!');
+    return $response;
+});
+
+$app->get('/products[/{name}]', function (Request $request, Response $response, array $args): Response {
+    $limit = $request->getQueryParams()['limit'] ?? 10;
+    $name = $args['name'] ?? 'CPU';
+    $response->getBody()->write("{$limit} products with the name {$name}.");
+    return $response;
 });
 
 $app->run();
